db32c8ce2f13df5f6aeba217f91ec18f0af53b7e
gustavo <lgmasco@gmail.com>
2015-09-09 14:21:54 -0300

    default nil payload to array

--- lib/geocodio/state_legislative_district.rb
+++ lib/geocodio/state_legislative_district.rb
@@ -4,6 +4,7 @@ module Geocodio
     attr_accessor :district_number
 
     def initialize(payload = {})
+      payload = {} if payload.nil?
       @name            = payload['name']
       @district_number = payload['district_number'] || 0
     end
